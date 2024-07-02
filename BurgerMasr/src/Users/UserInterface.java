3f0122f436983cd66ec04c749c47df30f287365b
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2024-07-02 03:36:57 +0300

    Update UserInterface

--- BurgerMasr/src/Users/UserInterface.java
+++ BurgerMasr/src/Users/UserInterface.java
@@ -1,11 +1,18 @@
 package Users;
 import javax.swing.text.View;
 
-public abstract class UserInterface {
-    public void viewMenue(){
+import food.MenuesDB;
 
+public abstract class UserInterface {
+    private MenuesDB db;
+    public void viewMenue(){
+        db = new MenuesDB();     
+        // Display Menu
+        System.out.println("Sandwich Menu:");
+        System.out.println(db.getMenuOfSandwiches());
     }
     public void viewAdditions(){
-
+        System.out.println("Extras Menu:");
+        System.out.println(db.getMenuOfExtras());
     }
 }
